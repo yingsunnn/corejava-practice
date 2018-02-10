15a9f7332a494d5daf70aa85db0d34eafdfb62c9
Ying <sco.suen@gmail.com>
2018-02-10 00:20:07 -0500

    feat: lambda

--- src/main/java/com/ying/lambda/collection/CollectionNewMethodTest.java
+++ src/main/java/com/ying/lambda/collection/CollectionNewMethodTest.java
@@ -1,7 +1,6 @@
 package com.ying.lambda.collection;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CollectionNewMethodTest {
     public static void main(String[] args) {
@@ -12,6 +11,35 @@ public class CollectionNewMethodTest {
 
         System.out.println(list);
 
-        
+        list.removeIf(string -> true);
+        System.out.println(list);
+
+        list.spliterator();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "a");
+        map.merge("2", "b", (v1, v2) -> v1 + v2);
+        map.merge("2", "c", (v1, v2) -> {
+            System.out.println("v1:" + v1);
+            System.out.println("v2:" + v2);
+            return null;
+        });
+        System.out.println(map);
+
+        Map<Integer, Set<String>> map1 = new HashMap<>();
+        map1.computeIfAbsent(1, v -> new HashSet<String>()).add("yi");
+        map1.computeIfAbsent(1, v -> new HashSet<String>()).add("er");
+        System.out.println(map1);
+
+        Map<Integer, Set<String>> map2 = new HashMap<>();
+        map2.compute(1, (key, value) -> new HashSet<String>()).add("one");
+        map2.compute(1, (key, value) -> new HashSet<String>()).add("two");
+        map2.compute(1, (key, value) -> null);
+
+
+        System.out.println("map2");
+        System.out.println(map2);
+
+
     }
 }
